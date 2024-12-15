c95fe55a008534c892c66e3ca3fdf137c27c3e5d
Apur0y <apuroy2785@gmail.com>
2024-12-15 23:55:38 +0600

    add  job

--- src/pages/Home/TopJobs.jsx
+++ src/pages/Home/TopJobs.jsx
@@ -4,6 +4,9 @@ import JobsCard from '../JobsCard';
 const TopJobs = () => {
     return (
         <div>
+            <div className='text-3xl font-semibold'>
+                Job Card
+            </div>
             <JobsCard></JobsCard>
         </div>
     );
